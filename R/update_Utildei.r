c276f2a09028b652a83d98c93b05521c67fa18b5
Ananya Roy Chowdhury <ananya.rc94@tamu.edu>
2021-04-28 12:43:51 -0500

    Changed the argument names in the function Ui

--- R/update_Utildei.r
+++ R/update_Utildei.r
@@ -1,65 +1,65 @@
-update_Utildei <- function(Utildei,beta,Wtildei,iSigmae,
-                           Ni,isnever,didconsume,Xtildei,mmi,iSigmau,n){
+update_Utildei <- function(UtildeiNI,betaNI,WtildeiNI,iSigmaeNI,
+                           NI_U,isneverNI,didconsumeNI,XtildeiNI,mmiNI,iSigmauNI,n){
 
 
 ###########################################################################
-# Update Utildei. This is done in two steps. In the first step, we generate
+# Update UtildeiNI. This is done in two steps. In the first step, we generate
 # it assuming that everyone is a consumer. In the second step, those who
 # are never consumers, i.e., Ni < 0, have their values updated by a
 # Metropolis step.
 ###########################################################################
 #
 # INPUT
-# Utildei        = the latent person-specific effects
-# beta           = the current values of all the betas
-# Wtildei        = the latent and non-latent responses
-# iSigmae        = inverse of Sigmae
-# Ni             = the crucial random variable
-# isnever        = Indicator that Ni < 0 and the person is a never consumer
-# didconsume     = Indicator that the person consumed on one of the
+# UtildeiNI        = the latent person-specific effects
+# betaNI           = the current values of all the betaNIs
+# WtildeiNI        = the latent and non-latent responses
+# iSigmaeNI        = inverse of Sigmae
+# NI_U             = the crucial random variable
+# isneverNI        = Indicator that Ni < 0 and the person is a never consumer
+# didconsumeNI     = Indicator that the person consumed on one of the
 #                     recalls
-# Xtildei        = design matrix
-# mmi            = # of recalls (assumed same for all)
-# iSigmau        = inverse of Sigmaeu
+# XtildeiNI        = design matrix
+# mmiNI            = # of recalls (assumed same for all)
+# iSigmauNI        = inverse of Sigmaeu
 
 
 # Here is Step 1
-Utildei_Current <- Utildei
-qq <- (Xtildei[ , ,1] %*% beta[ ,1])
+UtildeiNI_Current <- UtildeiNI
+qq <- (XtildeiNI[ , ,1] %*% betaNI[ ,1])
 for (jj in 2:3){
-    qq <- cbind(qq,  (Xtildei[ , ,jj] %*% beta[ ,jj]))
+    qq <- cbind(qq,  (XtildeiNI[ , ,jj] %*% betaNI[ ,jj]))
 }
 
 
-y = array(1, c(nrow(qq), ncol(qq), mmi))
-for( i in 1:mmi){
+y = array(1, c(nrow(qq), ncol(qq), mmiNI))
+for( i in 1:mmiNI){
   y[ , , i] = qq
 }
-ss <- Wtildei - y
+ss <- WtildeiNI - y
 
 
-c1  <- t(iSigmae%*% t(rowSums(ss, dims = 2))) 
-c2  <- pracma::inv(iSigmau + (mmi * iSigmae))
+c1  <- t(iSigmaeNI%*% t(rowSums(ss, dims = 2))) 
+c2  <- pracma::inv(iSigmauNI + (mmiNI * iSigmaeNI))
 #c2  = (c2+ c2')./2;
 # Here is Step 2
-isnever <- (Ni < 0)
+isneverNI <- (NI_U < 0)
 
-Utildei <- t(c2 %*% t(c1)) + (pracma::randn(n,3)  %*% pracma::sqrtm(c2)$B)
-c2_cand <- 1 / ((1 - pnorm(qq[ ,1] + Utildei[ ,1])) ^ mmi)
-c2_curr <- 1 / ( (1 - pnorm(qq[ ,1] + Utildei_Current[ ,1])) ^ mmi)
+UtildeiNI <- t(c2 %*% t(c1)) + (pracma::randn(n,3)  %*% pracma::sqrtm(c2)$B)
+c2_cand <- 1 / ((1 - pnorm(qq[ ,1] + UtildeiNI[ ,1])) ^ mmiNI)
+c2_curr <- 1 / ( (1 - pnorm(qq[ ,1] + UtildeiNI_Current[ ,1])) ^ mmiNI)
 rri     <- (pracma::rand(n,1) < (c2_cand / c2_curr))
-ddff       <- pracma::size(Utildei,2)
-Utildei_MH <- (Utildei * (rri %*% pracma::ones(1,ddff)) ) +
-             (Utildei_Current * ((1 - rri) %*% pracma::ones(1,ddff)))
+ddff       <- pracma::size(UtildeiNI,2)
+UtildeiNI_MH <- (UtildeiNI * (rri %*% pracma::ones(1,ddff)) ) +
+             (UtildeiNI_Current * ((1 - rri) %*% pracma::ones(1,ddff)))
 
-Utildei_new <- (Utildei * ((1 - isnever) %*% pracma::ones(1,ddff))) +
-                (Utildei_MH * (isnever %*% pracma::ones(1,ddff)))
+UtildeiNI_new <- (UtildeiNI * ((1 - isneverNI) %*% pracma::ones(1,ddff))) +
+                (UtildeiNI_MH * (isneverNI %*% pracma::ones(1,ddff)))
 
-for(i in 1:nrow(Utildei_new)){
-  for( j in 1:ncol(Utildei_new)){
-    Utildei_new[i, j] = ifelse(is.na(Utildei_new[i, j]), 0,Utildei_new[i, j] )
+for(i in 1:nrow(UtildeiNI_new)){
+  for( j in 1:ncol(UtildeiNI_new)){
+    UtildeiNI_new[i, j] = ifelse(is.na(UtildeiNI_new[i, j]), 0,UtildeiNI_new[i, j] )
   }
 } 
-return(Utildei_new)
+return(UtildeiNI_new)
 }
 
