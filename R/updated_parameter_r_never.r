230ac6f05297de9dfedcaebd495012ed9cb3752a
Ananya Roy Chowdhury <ananya.rc94@tamu.edu>
2021-04-29 01:45:00 -0500

    Made minor changes to the update r function and added checks for the rcpp versions

--- R/updated_parameter_r_never.r
+++ R/updated_parameter_r_never.r
@@ -37,16 +37,14 @@ if(isTRUE(all.equal(rcurr, -0.99))){
     #print(paste("this is ss:", ss,"when rcurr = ",rcurr,"and rcand = ",rcand,"and spacing = ",spacing," and i = ", i))
     
    
-}
-else if(isTRUE(all.equal(rcurr, 0.99))){
+} else if(isTRUE(all.equal(rcurr, 0.99))){
     i = 2
     ss    <- pracma::rand(1,1)
     rcand <- (rcurr * as.numeric(ss <= 0.33)) + ((rcurr - spacing)
             * as.numeric(ss > 0.33) * as.numeric(ss <= 0.66)) +
             + ((rcurr - (2 * spacing)) * as.numeric(ss > 0.66))
     #print(paste("this is ss:", ss,"when rcurr = ",rcurr,"and rcand = ",rcand,"and spacing = ",spacing," and i = ", i))
-}
-else{
+} else{
     # if(rcurr > -0.99){
     #     if(rcurr < 0.99){
     i = 3
@@ -68,7 +66,7 @@ else{
 
 #print(paste("this is rcand =",rcand,"before we call formGOF"))
 GofSigmaecurr = formGofSigmae_never(rcurr,theta,s22,s33,qq,mmi)
-GofSigmaecand = formGofSigmae_never_c(rcand,theta,s22,s33,qq,mmi)
+GofSigmaecand = formGofSigmae_never(rcand,theta,s22,s33,qq,mmi)
 
 gg            <- GofSigmaecand - GofSigmaecurr
 gg            <- gg - ((mmi*n/2) * log(1 - (rcand ^ 2))) + ((mmi*n/2)
@@ -93,4 +91,15 @@ rnew          <- (rcand * (ss1 < gg)) + ((rcurr * (ss1 > gg)))
 return(rnew)
 }
 
+# g_r = g_c = vector()
+# for(i in 1:1000) {
+#   g_r[i] = formGofSigmae_never(rcurr,theta,s22,s33,qq,mmi)
+#   g_c[i] = formGofSigmae_never_c(rcurr,theta,s22,s33,qq,mmi)
+# }
+
+# r_r = r_c = vector()
+# for(i in 1:10000) {
+#   r_r[i] = updated_parameter_theta_never(r,theta,s22,s33,qq,mmi)
+#   r_c[i] = updated_parameter_theta_never_c(r,theta,s22,s33,qq,mmi)
+# }
 
